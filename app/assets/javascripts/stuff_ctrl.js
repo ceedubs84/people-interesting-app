c689420e60f5da2b1aa0db707d850a524e20cd56
Chris Woodside <christopherwoodside@gmail.com>
2016-12-20 20:41:18 -0600

    Updated JS controllers

--- app/assets/javascripts/stuff_ctrl.js
+++ app/assets/javascripts/stuff_ctrl.js
@@ -2,14 +2,30 @@
   "use strict";
 
   angular.module("app").controller("stuffCtrl", function($scope) {
-    var users = [
-      {name: "Buster Bluth", bio: "Student, youngest son", bioVisible: false},
-      {name: "George Bluth", bio: "Former CEO of Bluth Company", bioVisible: false},
-      {name: "Lucille Bluth", bio: "Matriarch of Bluth family", bioVisible: false},
+    $scope.users = [
+      {name: "Buster Bluth", bio: "Student, youngest son", bioVisible: true},
+      {name: "George Bluth", bio: "Former CEO of Bluth Company", bioVisible: true},
+      {name: "Lucille Bluth", bio: "Matriarch of Bluth family", bioVisible: true},
     ];
+
+    $scope.addUser = function(inputName, inputBio, inputBioVisible) {
+      var newUser = {name: inputName, bio: inputBio, bioVisible: false};
+      $scope.users.push(newUser);
+    };
+
+    $scope.isVisible = function(user) {
+      if (user.bioVisible.indexOf("false") === -1) {
+        user.bioVisible = "true";
+      } else if (user.bioVisible.indexOf("true") === -1) {
+        user.bioVisible = "false";
+      }
+    };
     
-    $scope.message = "Hello!";
-    $scope.myName = "Slim Shady";
+
+    $scope.bioToggle = function(user) {
+      if (user.bioVisible)
+    };
+
     $scope.sum = 343 + 5353;
   });
     
