2af4cd7bdc9c9f8329b20272ab3623524735799d
Chris Woodside <christopherwoodside@gmail.com>
2016-12-20 18:52:44 -0600

    Added Users Index and Show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
-
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
+  get '/' => 'users#index'
+  get '/users' => 'users#index'
+  get '/users/:id' => 'users#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
